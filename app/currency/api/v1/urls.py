5949c8cdebbf6d98d3d1a9e08945d632fafd1b18
Nikita Stoliarov <stlrvnk@gmail.com>
2023-10-04 22:34:51 +0300

    Comments added

--- app/currency/api/v1/urls.py
+++ app/currency/api/v1/urls.py
@@ -13,21 +13,25 @@ router = DefaultRouter(trailing_slash=False)
 urlpatterns = [
     path("", include(router.urls)),
 
+    # Rate model endpoints
     path("rate/list", RateListAPIView.as_view(), name="rate-list"),
     path("rate/create/", RateCreateAPIView.as_view(), name="rate-create"),
     path("rate/<int:pk>/", RateDetailedAPIView.as_view(),
          name="rate-retrieve-update-destroy"),
 
+    # Source model endpoints
     path("source/list", SourceListAPIView.as_view(), name="source-list"),
     path("source/create/", SourceCreateAPIView.as_view(), name="source-create"),
     path("source/<int:pk>/", SourceDetailedAPIView.as_view(),
          name="source-retrieve-update-destroy"),
 
+    # ContactUs model endpoints
     path("contactus/list", ContactUsListAPIView.as_view(), name="contactus-list"),
     path("contactus/create/", ContactUsCreateAPIView.as_view(), name="contactus-create"),
     path("contactus/<int:pk>/", ContactUsDetailedAPIView.as_view(),
          name="contactus-retrieve-update-destroy"),
 
+    # Logs model endpoints
     path("logs/list", LogsListAPIView.as_view(), name="logs-list"),
     path("logs/<int:pk>/", LogsDetailedAPIView.as_view(),
          name="logs-retrieve-update-destroy"),
