ee0cea892865db40daa11683817d39dc22d34e41
zahdyma <102221249+zdima656995zaharchenko@users.noreply.github.com>
2023-10-06 17:49:10 +0300

    tests

--- app/tests/api.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.test import APIClient
-
-
-def test_get_api_rates():
-    api_client = APIClient()
-    response = api_client.get("/api/v1/currency/rates/")
-    assert response.status_code == 200
--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -6,3 +6,36 @@ def enable_db_access_for_all_tests(db):
     """
     give access to database for all tests
     """
+
+
+@pytest.fixture()
+def api_client():
+    from rest_framework.test import APIClient
+    client = APIClient()
+    yield client
+
+
+@pytest.fixture(scope="function")
+def api_client_auth(django_user_model):
+    from rest_framework.test import APIClient
+    client = APIClient()
+
+
+    password = 'superSecretPassword'
+    user = django_user_model(
+        email='example@admin.com',
+    )
+    user.set_password(password)
+    user.save()
+
+    token_response = api_client.post(
+        '/api/v1/token',
+        data={'email': user.email, 'password': password},
+    )
+    assert token_response.status_code == 200
+    access = token_response.json()['access']
+    api_client.credentials(HTTP_AUTHORIZATION=f'JWT {access}')
+
+    yield api_client
+
+    user.delete()
